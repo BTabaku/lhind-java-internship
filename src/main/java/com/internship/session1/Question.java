b8bd0d6f18d7a0e7f7435b04ea9db3a6bf15692f
BTabaku <baftit40@gmail.com>
2025-02-14 15:26:38 +0100

    Feature branch created based on the improvement required , also made use of "StringUtils.isNotBlank"

--- src/main/java/com/internship/session1/Question.java
+++ src/main/java/com/internship/session1/Question.java
@@ -1,35 +1,41 @@
 package com.internship.session1;
 
-    import java.util.HashMap;
-    import java.util.Map;
+import org.apache.commons.lang3.StringUtils;
 
-    public class Question {
-        private String question;
-        private Map<Integer, Integer> answerCounts;
+import java.util.HashMap;
+import java.util.Map;
 
-        public Question(String question) {
+public class Question {
+    private String question;
+    private Map<Integer, Integer> answerCounts;
+
+    public Question(String question) {
+        if (StringUtils.isNotBlank(question)) {
             this.question = question;
-            this.answerCounts = new HashMap<>();
-            for (int i = 0; i < 4; i++) {
-                answerCounts.put(i, 0);
-            }
+        } else {
+            throw new IllegalArgumentException("Question text cannot be blank");
         }
-
-        public String getQuestion() {
-            return question;
-        }
-
-        public void recordAnswer(int answer) {
-            if (answerCounts.containsKey(answer)) {
-                answerCounts.put(answer, answerCounts.get(answer) + 1);
-            }
-        }
-
-        public int[] getAnswers() {
-            int[] answers = new int[4];
-            for (int i = 0; i < 4; i++) {
-                answers[i] = answerCounts.getOrDefault(i, 0);
-            }
-            return answers;
+        this.answerCounts = new HashMap<>();
+        for (int i = 0; i < 4; i++) {
+            answerCounts.put(i, 0);
         }
     }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void recordAnswer(int answer) {
+        if (answerCounts.containsKey(answer)) {
+            answerCounts.put(answer, answerCounts.get(answer) + 1);
+        }
+    }
+
+    public int[] getAnswers() {
+        int[] answers = new int[4];
+        for (int i = 0; i < 4; i++) {
+            answers[i] = answerCounts.getOrDefault(i, 0);
+        }
+        return answers;
+    }
+}
--- src/main/java/com/internship/session1/Survey.java
+++ src/main/java/com/internship/session1/Survey.java
@@ -1,46 +1,52 @@
 package com.internship.session1;
 
-        import java.util.ArrayList;
-        import java.util.HashSet;
-        import java.util.List;
-        import java.util.Set;
+import org.apache.commons.lang3.StringUtils;
 
-        public class Survey {
-            private String title;
-            private List<Question> questions;
-            private List<Candidate> candidates;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-            public Survey(String title) {
-                this.title = title;
-                this.questions = new ArrayList<>();
-                this.candidates = new ArrayList<>();
-            }
+public class Survey {
+    private String title;
+    private List<Question> questions;
+    private List<Candidate> candidates;
 
-            public String getTitle() {
-                return title;
-            }
-
-            public List<Question> getQuestions() {
-                return questions;
-            }
-
-            public List<Candidate> getCandidates() {
-                return candidates;
-            }
-
-            public void addQuestion(Question question) {
-                questions.add(question);
-            }
-
-            public void addCandidate(Candidate candidate) {
-                candidates.add(candidate);
-            }
-
-            public boolean isValid() {
-                if (questions.size() < 10 || questions.size() > 40) {
-                    return false;
-                }
-                Set<Question> uniqueQuestions = new HashSet<>(questions);
-                return uniqueQuestions.size() == questions.size();
-            }
+    public Survey(String title) {
+        if (StringUtils.isNotBlank(title)) {
+            this.title = title;
+        } else {
+            throw new IllegalArgumentException("Survey title cannot be blank");
         }
+        this.questions = new ArrayList<>();
+        this.candidates = new ArrayList<>();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public List<Candidate> getCandidates() {
+        return candidates;
+    }
+
+    public void addQuestion(Question question) {
+        questions.add(question);
+    }
+
+    public void addCandidate(Candidate candidate) {
+        candidates.add(candidate);
+    }
+
+    public boolean isValid() {
+        if (questions.size() < 10 || questions.size() > 40) {
+            return false;
+        }
+        Set<Question> uniqueQuestions = new HashSet<>(questions);
+        return uniqueQuestions.size() == questions.size();
+    }
+}
