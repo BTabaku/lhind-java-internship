93be719c50f9277808daba81104d9b131832576a
BTabaku <baftit40@gmail.com>
2025-02-24 17:57:07 +0100

    Added spring boot support

---
+++ session6SpringBoot/src/test/java/com/internship/session6springboot/Session6SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internship.session6springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Session6SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
