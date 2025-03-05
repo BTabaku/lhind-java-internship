f429eb1061015e5b75276fa2a3030a2a2436db1e
BTabaku <baftit40@gmail.com>
2025-03-05 22:40:44 +0100

    Initialize main application and add basic test structure

--- internshipfinalproject/src/main/java/com/lhind/internshipfinalproject/InternshipFinalProjectApplication.java
+++ internshipfinalproject/src/main/java/com/lhind/internshipfinalproject/InternshipFinalProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InternshipfinalprojectApplication {
+public class InternshipFinalProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InternshipfinalprojectApplication.class, args);
+		SpringApplication.run(InternshipFinalProjectApplication.class, args);
 
 		System.out.println("Hello World");
 	}
