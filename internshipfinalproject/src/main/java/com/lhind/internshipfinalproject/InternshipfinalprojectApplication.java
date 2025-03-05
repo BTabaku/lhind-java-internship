1de66c8b10e1760d388c7bc3953bf67ead344da7
BTabaku <baftit40@gmail.com>
2025-03-05 17:08:41 +0100

    Add greeting message to main application

--- internshipfinalproject/src/main/java/com/lhind/internshipfinalproject/InternshipfinalprojectApplication.java
+++ internshipfinalproject/src/main/java/com/lhind/internshipfinalproject/InternshipfinalprojectApplication.java
@@ -8,6 +8,8 @@ public class InternshipfinalprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InternshipfinalprojectApplication.class, args);
+
+		System.out.println("Hello World");
 	}
 
 }
